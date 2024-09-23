b2d46cf11154b953978e870f7bd24c6a84ad7747
zizdlp <zdlp@sina.cn>
2024-09-23 17:27:32 +0800

    add hadoop

--- ansible/files/hadoop-env.sh
+++ ansible/files/hadoop-env.sh
@@ -4,5 +4,5 @@ export HDFS_DATANODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
 export YARN_RESOURCEMANAGER_USER=root
 export YARN_NODEMANAGER_USER=root
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export HADOOP_OPTS="--add-opens=java.base/java.lang=ALL-UNNAMED $HADOOP_OPTS"
