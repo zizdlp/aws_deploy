17a97ba32b6eff18d4bd8e5f9139a0f80e967749
zizdlp <zdlp@sina.cn>
2024-09-23 17:47:34 +0800

    add ssh

--- ansible/files/hadoop-env.sh
+++ ansible/files/hadoop-env.sh
@@ -6,3 +6,4 @@ export YARN_RESOURCEMANAGER_USER=root
 export YARN_NODEMANAGER_USER=root
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export HADOOP_OPTS="--add-opens=java.base/java.lang=ALL-UNNAMED $HADOOP_OPTS"
+export HADOOP_SSH_OPTS="-i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
