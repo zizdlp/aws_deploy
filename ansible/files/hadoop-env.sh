b46d07966880ed715758559e7f9580da9374640a
zizdlp <zdlp@sina.cn>
2024-09-24 09:31:48 +0800

    update

--- ansible/files/hadoop-env.sh
+++ ansible/files/hadoop-env.sh
@@ -1,4 +1,3 @@
-
 export HDFS_NAMENODE_USER=root
 export HDFS_DATANODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
@@ -6,4 +5,4 @@ export YARN_RESOURCEMANAGER_USER=root
 export YARN_NODEMANAGER_USER=root
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export HADOOP_OPTS="--add-opens=java.base/java.lang=ALL-UNNAMED $HADOOP_OPTS"
-export HADOOP_SSH_OPTS="-i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
+export HADOOP_SSH_OPTS="-i /root/.ssh/id_rsa -o StrictHostKeyChecking=no"
