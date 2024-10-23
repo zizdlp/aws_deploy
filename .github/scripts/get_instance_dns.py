8454b9512bf4a10d7165a965a56a214ebd80c38d
zizdlp <zdlp@sina.cn>
2024-10-23 09:36:10 +0800

    save info

--- .github/scripts/get_instance_dns.py
+++ .github/scripts/get_instance_dns.py
@@ -1,6 +1,6 @@
 import boto3
 import subprocess
-
+import argparse
 # 初始化 EC2 客户端
 ec2 = boto3.client('ec2')
 
@@ -26,25 +26,36 @@ def get_commit_hash():
     # 获取当前 Git commit hash
     commit_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip().decode('utf-8')
     return commit_hash
+def save_info(runner):
+    # 获取标签键为 Name 且值完全匹配 SparkNode-{commit_hash} 的实例
+    commit_hash = get_commit_hash()  # 获取当前 commit hash
+    tag_value = f'SparkNode-{commit_hash}-{runner}'  # 定义要搜索的标签值
+    instances = get_instances_by_tag('Name', tag_value)
 
-# 获取标签键为 Name 且值完全匹配 SparkNode-{commit_hash} 的实例
-commit_hash = get_commit_hash()  # 获取当前 commit hash
-tag_value = f'SparkNode-{commit_hash}'  # 定义要搜索的标签值
-instances = get_instances_by_tag('Name', tag_value)
+    # 保存节点信息到一个文件，包含 Public IP 地址
+    with open('./.github/scripts/nodes_info.txt', 'w') as f:
+        for instance in instances:
+            # 查找标签中的 Index
+            tags = {tag['Key']: tag['Value'] for tag in instance.get('Tags', [])}
+            index = tags.get('Index', 'unknown')
 
-# 保存节点信息到一个文件，包含 Public IP 地址
-with open('./.github/scripts/nodes_info.txt', 'w') as f:
-    for instance in instances:
-        # 查找标签中的 Index
-        tags = {tag['Key']: tag['Value'] for tag in instance.get('Tags', [])}
-        index = tags.get('Index', 'unknown')
+            # 提取 Public DNS、Private IP 和 Public IP
+            public_dns = instance.get('PublicDnsName', 'No Public DNS assigned')
+            private_ip = instance.get('PrivateIpAddress', 'No Private IP assigned')
+            public_ip = instance.get('PublicIpAddress', 'No Public IP assigned')
 
-        # 提取 Public DNS、Private IP 和 Public IP
-        public_dns = instance.get('PublicDnsName', 'No Public DNS assigned')
-        private_ip = instance.get('PrivateIpAddress', 'No Private IP assigned')
-        public_ip = instance.get('PublicIpAddress', 'No Public IP assigned')
+            # 保存节点信息到文件，每一行格式为：node{index} {Public DNS} {Private IP} {Public IP}
+            f.write(f'node{index} {public_dns} {private_ip} {public_ip}\n')
 
-        # 保存节点信息到文件，每一行格式为：node{index} {Public DNS} {Private IP} {Public IP}
-        f.write(f'node{index} {public_dns} {private_ip} {public_ip}\n')
+    print('Node information with Public IPs has been saved to ./.github/scripts/nodes_info.txt')
 
-print('Node information with Public IPs has been saved to ./.github/scripts/nodes_info.txt')
+
+# Main function to parse command line arguments
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='save node info')
+    parser.add_argument('--runner', type=str, default='all', help='runner case for spark or ?')
+    
+    args = parser.parse_args()
+    
+    # Launch instances based on command line inputs
+    save_info(args.runner)
