f0415fd477f7d1e1b3cbed45868e3deff0381521
zizdlp <zdlp@sina.cn>
2024-10-23 09:40:44 +0800

    333

--- tests/case0.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("TestJob").getOrCreate()
-data = [("Alice", 29), ("Bob", 31), ("Catherine", 25)]
-df = spark.createDataFrame(data, ["Name", "Age"])
-df.show()
-
-spark.stop()
