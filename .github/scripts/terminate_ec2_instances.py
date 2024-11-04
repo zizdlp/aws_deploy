053f0eff095973c24d39f67fe141d86c0f4f9aa9
zizdlp <zdlp@sina.cn>
2024-11-04 18:31:51 +0800

    a

--- .github/scripts/terminate_ec2_instances.py
+++ .github/scripts/terminate_ec2_instances.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     parser.add_argument('--region', type=str, default="us-west-1", help='实例所在区域')
     parser.add_argument('--run-number', type=str, default="0", help='github action run number')
     parser.add_argument('--run-type', type=str, default="spark", help='运行任务类型')
-    parser.add_argument('--ignore', type=bool, default=False, help='是否应该忽略，而不终止实例')
     args = parser.parse_args()
-    terminate_instances(args.region,args.run_type,args.run_number,args.ignore)
+    terminate_instances(args.region,args.run_type,args.run_number)
