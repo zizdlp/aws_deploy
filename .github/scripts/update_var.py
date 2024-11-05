716995eb42dba82c6310a1892cfb5ca3dc558130
zizdlp <zdlp@sina.cn>
2024-11-05 11:45:21 +0800

    udpate

--- .github/scripts/update_var.py
+++ .github/scripts/update_var.py
@@ -3,63 +3,72 @@ import stat
 import xml.etree.ElementTree as ET
 
 # 定义要匹配并替换/追加的变量及其默认值
-variables = {
-    "scaleFactor": os.environ.get("SCALE_FACTOR", 100),  # 从环境变量中获取
-    "spark_numexecutors": os.environ.get("SPARK_NUM_EXECUTORS", 3),
-    "spark_executorcores": os.environ.get("SPARK_EXECUTOR_CORES", 12),
-    "spark_executormemory": os.environ.get("SPARK_EXECUTOR_MEMORY", "90g"),
-    "spark_offsize": os.environ.get("SPARK_OFFSIZE", "0g"),
-    "spark_overhead": os.environ.get("SPARK_OVERHEAD", "6g"),
-    "chukonu_numexecutors": os.environ.get("CHUKONU_NUM_EXECUTORS", 3),
-    "chukonu_executorcores": os.environ.get("CHUKONU_EXECUTOR_CORES", 12),
-    "chukonu_executormemory": os.environ.get("CHUKONU_EXECUTOR_MEMORY", "47g"),
-    "chukonu_offsize": os.environ.get("CHUKONU_OFFSIZE", "1g"),
-    "chukonu_overhead": os.environ.get("CHUKONU_OVERHEAD", "48g"),
+spark_variables = {
+    "spark.driver.memory":os.environ.get("SPARK_DRIVER_MEMORY", "4g"),
+    "spark.executor.cores": os.environ.get("SPARK_EXECUTOR_CORES", 12),
+    "spark.executor.memory": os.environ.get("SPARK_EXECUTOR_MEMORY", "90g"),
+    "spark.executor.memoryOverhead": os.environ.get("SPARK_OVERHEAD", "6g"),
 }
 
-# 读取现有的 var.sh 文件内容
-var_file_path = './ansible/files/var.sh'
+spark_conf_path = './ansible/files/spark-defaults-spark.conf'
 
-# 用于存储新的文件内容
-new_lines = []
 
-# 读取文件中的现有行
-if os.path.exists(var_file_path):
-    with open(var_file_path, 'r') as var_file:
-        existing_lines = var_file.readlines()
-else:
-    existing_lines = []
+chukonu_variables = {
+    "spark.driver.memoryOverhead":os.environ.get("CHUKONU_DRIVER_OVERHEAD", "4g"),
+    "spark.driver.memory":os.environ.get("CHUKONU_DRIVER_MEMORY", "4g"),
+    "spark.executor.cores": os.environ.get("CHUKONU_EXECUTOR_CORES", 12),
+    "spark.executor.memory": os.environ.get("CHUKONU_EXECUTOR_MEMORY", "48g"),
+    "spark.executor.memoryOverhead": os.environ.get("CHUKONU_OVERHEAD", "46g"),
+}
 
-# 标记哪些变量已匹配
-updated_vars = {var: False for var in variables}
+chukonu_conf_path = './ansible/files/spark-defaults-chukonu.conf'
 
-# 逐行检查并更新现有行
-for line in existing_lines:
-    updated = False
+def update_spark_conf(variables,conf_path):
+
+    # 用于存储新的文件内容
+    new_lines = []
+
+    # 读取文件中的现有行
+    if os.path.exists(conf_path):
+        with open(conf_path, 'r') as var_file:
+            existing_lines = var_file.readlines()
+    else:
+        existing_lines = []
+
+    # 标记哪些变量已匹配
+    updated_vars = {var: False for var in variables}
+
+    # 逐行检查并更新现有行
+    for line in existing_lines:
+        updated = False
+        for var_name, var_value in variables.items():
+            if line.startswith(f"{var_name}="):  # 匹配到以变量名开头的行
+                new_lines.append(f"{var_name}={var_value}\n")  # 用新的值替换
+                updated_vars[var_name] = True  # 标记为已更新
+                updated = True
+                print(f"Updated {var_name} to {var_value}")
+                break
+        if not updated:
+            new_lines.append(line)  # 保留未匹配到的行
+
+    # 追加未匹配到的变量（新变量）
     for var_name, var_value in variables.items():
-        if line.startswith(f"{var_name}="):  # 匹配到以变量名开头的行
-            new_lines.append(f"{var_name}={var_value}\n")  # 用新的值替换
-            updated_vars[var_name] = True  # 标记为已更新
-            updated = True
-            print(f"Updated {var_name} to {var_value}")
-            break
-    if not updated:
-        new_lines.append(line)  # 保留未匹配到的行
+        if not updated_vars[var_name]:
+            new_lines.append(f"{var_name}={var_value}\n")
+            print(f"Appended {var_name} with value {var_value}")
 
-# 追加未匹配到的变量（新变量）
-for var_name, var_value in variables.items():
-    if not updated_vars[var_name]:
-        new_lines.append(f"{var_name}={var_value}\n")
-        print(f"Appended {var_name} with value {var_value}")
+    # 写回 conf 文件
+    with open(conf_path, 'w') as var_file:
+        var_file.writelines(new_lines)
 
-# 写回 var.sh 文件
-with open(var_file_path, 'w') as var_file:
-    var_file.writelines(new_lines)
+    # 确保 conf 文件具有可执行权限
+    st = os.stat(conf_path)
+    os.chmod(conf_path, st.st_mode | stat.S_IEXEC)  # 添加执行权限
+    print(f"Set {conf_path} as an executable.")
 
-# 确保 var.sh 文件具有可执行权限
-st = os.stat(var_file_path)
-os.chmod(var_file_path, st.st_mode | stat.S_IEXEC)  # 添加执行权限
-print(f"Set {var_file_path} as an executable.")
+
+update_spark_conf(spark_variables,spark_conf_path)
+update_spark_conf(chukonu_variables,chukonu_conf_path)
 
 # 定义要替换的 YARN 配置属性及其默认值
 yarn_config_updates = {
